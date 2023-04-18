4ed4402def493e84948009475f901ffbf3e06fa6
zakzakst <e1433104@gmail.com>
2023-04-18 20:22:40 +0900

    model_basic

--- src/15_model_basic/script.ts
+++ src/15_model_basic/script.ts
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { TDSLoader } from "three/examples/jsm/loaders/TDSLoader";
+import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader";
 
 async function init() {
   const canvasEl = document.getElementById("canvas");
@@ -30,14 +32,32 @@ async function init() {
   directionalLight.position.set(1, 0, 1);
   scene.add(directionalLight2);
 
-  const loader = new GLTFLoader();
-  const gltf = await loader.loadAsync(
-    "https://ics-creative.github.io/tutorial-three/samples/models/gltf/gltf/ToyCar.gltf"
-  );
-  const model = gltf.scene;
-  scene.add(model);
+  // const loader = new GLTFLoader();
+  // // const gltf = await loader.loadAsync(
+  // //   "https://ics-creative.github.io/tutorial-three/samples/models/gltf/gltf/ToyCar.gltf"
+  // // );
+  // const gltf = await loader.loadAsync(
+  //   "https://ics-creative.github.io/tutorial-three/samples/models/gltf/binary/ToyCar.glb"
+  // );
+  // const model = gltf.scene;
+  // scene.add(model);
+  // model.scale.set(30, 30, 30);
 
-  model.scale.set(30, 30, 30);
+  // const loader = new TDSLoader();
+  // loader.setResourcePath(
+  //   "https://ics-creative.github.io/tutorial-three/samples/models/3ds/portalgun/textures/"
+  // );
+  // const object = await loader.loadAsync(
+  //   "https://ics-creative.github.io/tutorial-three/samples/models/3ds/portalgun/portalgun.3ds"
+  // );
+  // scene.add(object);
+
+  const loader = new ColladaLoader();
+  const collada = await loader.loadAsync(
+    "https://ics-creative.github.io/tutorial-three/samples/models/collada/elf/elf.dae"
+  );
+  const model = collada.scene;
+  scene.add(model);
 
   const tick = () => {
     renderer.render(scene, camera);
