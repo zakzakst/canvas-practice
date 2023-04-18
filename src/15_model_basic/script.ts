9ca33c68abe1070bc45bf14ac485373bd9cf8b04
zakzakst <e1433104@gmail.com>
2023-04-18 20:10:18 +0900

    model_basic

--- src/15_model_basic/script.ts
+++ src/15_model_basic/script.ts
@@ -1,17 +1,50 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const canvasEl = document.getElementById("canvas");
-const width = 1600;
-const height = 900;
+async function init() {
+  const canvasEl = document.getElementById("canvas");
+  const width = 1600;
+  const height = 900;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvasEl,
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width, height);
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasEl,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
 
-const scene = new THREE.Scene();
+  const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, width / height);
-camera.position.set(20, 20, 20);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+  camera.position.set(1, 1, 1);
+
+  const controls = new OrbitControls(camera, canvasEl);
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
+
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 0, 1);
+  scene.add(directionalLight2);
+
+  const loader = new GLTFLoader();
+  const gltf = await loader.loadAsync(
+    "https://ics-creative.github.io/tutorial-three/samples/models/gltf/gltf/ToyCar.gltf"
+  );
+  const model = gltf.scene;
+  scene.add(model);
+
+  model.scale.set(30, 30, 30);
+
+  const tick = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  };
+
+  tick();
+}
+
+init();
