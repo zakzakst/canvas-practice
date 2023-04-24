17e7ea3e383088e28946cd57bd611214b52c2f27
zakzakst <e1433104@gmail.com>
2023-04-24 12:11:57 +0900

    class

--- src/20_class/script.ts
+++ src/20_class/script.ts
@@ -13,5 +13,24 @@ renderer.setSize(width, height);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, width / height);
-camera.position.set(20, 20, 20);
+camera.position.set(-100, 150, 500);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
+class Donuts extends THREE.Mesh {
+  constructor() {
+    const geometry = new THREE.TorusGeometry(120, 40, 60, 50);
+    const material = new THREE.MeshNormalMaterial();
+    super(geometry, material);
+  }
+}
+
+const mesh = new Donuts();
+scene.add(mesh);
+
+const tick = () => {
+  mesh.rotation.x += 0.02;
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick);
+};
+
+tick();
