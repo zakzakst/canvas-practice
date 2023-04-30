28e05f481fd54b3a6593ae44247cd2c0d4e82d05
zakzakst <e1433104@gmail.com>
2023-04-30 19:22:49 +0900

    修正

---
+++ src/shader/script.ts
@@ -0,0 +1,83 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Canvas
+const canvas = document.querySelector(".webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+
+// Material
+const material = new THREE.MeshBasicMaterial();
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0.25, -0.25, 1);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const animate = () => {
+  //時間取得
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(animate);
+};
+
+animate();
