7c8b4674f66d006f45eff20c1439168b4ee7797e
zakzakst <e1433104@gmail.com>
2023-09-19 16:54:22 +0900

    snippet追記

--- snippet-memo/make-data.ts
+++ snippet-memo/make-data.ts
@@ -1,6 +1,5 @@
 // 【内容】
 // 指定の数の配列データを作成する
-
 type ItemData = {
   id: number;
   name: string;
@@ -12,3 +11,14 @@ const items: ItemData[] = [...new Array(total)].map((_, index) => ({
   id: index,
   name: `name-${index}`,
 }));
+
+// 【応用】
+// アルファベットの配列を作成する
+
+// 「a」のUTF-16コードを取得
+const start = "a".charCodeAt(0);
+
+const alphabets = Array.apply(null, new Array(26)).map((_, index) => {
+  // UTF-16コードから文字列を取得
+  return String.fromCharCode(start + index);
+});
