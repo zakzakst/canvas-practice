35bc45ea8736a7fad4017e14ef174c2bf461abf8
zakzakst <e1433104@gmail.com>
2023-09-19 17:04:30 +0900

    修正

--- snippet-memo/01-make-data.js
+++ snippet-memo/01-make-data.js
@@ -1,24 +1,27 @@
+// 【参考】
+// https://qiita.com/reika_engineer/items/b8b6ca343cf33157a666
+
 // 【内容】
 // 指定の数の配列データを作成する
-type ItemData = {
-  id: number;
-  name: string;
-};
-
 const total = 20;
 
-const items: ItemData[] = [...new Array(total)].map((_, index) => ({
+const items = [...new Array(total)].map((_, index) => ({
   id: index,
   name: `name-${index}`,
 }));
 
-// 【応用】
+console.log(items);
+
+// 【内容】
 // アルファベットの配列を作成する
 
 // 「a」のUTF-16コードを取得
 const start = "a".charCodeAt(0);
 
+// Array.applyを利用した書き方もある（ちょっと理解しきれてないが。。）
 const alphabets = Array.apply(null, new Array(26)).map((_, index) => {
   // UTF-16コードから文字列を取得
   return String.fromCharCode(start + index);
 });
+
+console.log(alphabets);
---
+++ snippet-memo/02-sort.js
@@ -0,0 +1,7 @@
+// 【参考】
+// https://kinocolog.com/javascript_sort/
+
+// 【内容】
+// 指定の数の配列データを作成する
+
+console.log("test");
