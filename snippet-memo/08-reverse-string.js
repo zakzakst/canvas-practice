9f7b08c6c56ea573cfbe65a3f5e5d52b3f1811f8
zakzakst <e1433104@gmail.com>
2023-09-26 08:16:04 +0900

    修正

--- snippet-memo/08-reverse-string.js
+++ snippet-memo/08-reverse-string.js
@@ -2,3 +2,31 @@
 // https://www.freecodecamp.org/japanese/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
 
 // 【内容：文字列を反転させる】
+
+/**
+ * 反転した文字列を取得する
+ *
+ * @function
+ * @param {string[]} str 元となる文字列
+ * @returns {string[]} 反転した文字列
+ */
+const getReverseString = (str) => {
+  return str.split("").reverse().join("");
+};
+
+const getReverseString2 = (str) => {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+};
+
+const getReverseString3 = (str) => {
+  if (str === "") return "";
+  return getReverseString3(str.substr(1)) + str.charAt(0);
+};
+
+console.log(getReverseString("test"));
+console.log(getReverseString2("test"));
+console.log(getReverseString3("test"));
