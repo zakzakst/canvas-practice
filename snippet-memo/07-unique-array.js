9c07b9d91905f921623838a40208f22a817c6b18
zakzakst <e1433104@gmail.com>
2023-09-26 02:10:04 +0900

    修正

--- snippet-memo/07-unique-array.js
+++ snippet-memo/07-unique-array.js
@@ -2,3 +2,19 @@
 // https://qiita.com/netebakari/items/7c1db0b0cea14a3d4419
 
 // 【内容：配列の重複排除】
+
+/**
+ * 重複排除した配列を取得する
+ *
+ * @function
+ * @param {string[]} arr 元となる配列
+ * @returns {string[]} 重複排除した配列
+ */
+const getUniqueArray = (arr) => {
+  return Array.from(new Set(arr));
+
+  // よくない書き方だけど参考に覚えておく
+  // return arr.filter((item, index) => arr.indexOf(item) === index);
+};
+
+console.log(getUniqueArray(["a", "b", "d", "e", "a", "d"]));
---
+++ snippet-memo/08-reverse-string.js
@@ -0,0 +1,4 @@
+// 【参考】
+// https://www.freecodecamp.org/japanese/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+
+// 【内容：文字列を反転させる】
