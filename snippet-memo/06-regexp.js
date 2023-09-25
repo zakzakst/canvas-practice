9bc5dfa2669a52cad226629ab3037a38fe9b7525
zakzakst <e1433104@gmail.com>
2023-09-26 01:51:05 +0900

    修正

--- snippet-memo/06-regexp.js
+++ snippet-memo/06-regexp.js
@@ -1,4 +1,35 @@
 // 【参考】
 // https://javascript.programmer-reference.com/js-regexp-sample/
+// https://mebee.info/2020/09/20/post-18756/
 
 // 【内容：正規表現パターンサンプル】
+const REGS = {
+  num: /^[0-9]*$/,
+  numNoBlank: /^[0-9]+$/,
+  alphabet: /^[a-zA-Z]*$/,
+  numNoBlank: /^[a-zA-Z]+$/,
+  numAlphabet: /^[0-9a-zA-Z]*$/,
+  numAlphabetSign: /^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/,
+  url: /^https?:\/\//,
+  postCode: /^\d{3}-\d{4}$/,
+  ip: /^\d{1,3}(\.\d{1,3}){3}$/,
+  decimal: /^([1-9][0-9]*|0)(\.[0-9]+)?$/,
+  decimal2: /^-?([1-9][0-9]*|0)(\.[0-9]+)?$/,
+  zenHira: /^[ぁ-んー]*$/,
+  zenKana: /^[ァ-ンヴー]*$/,
+  hanKana: /^[ｧ-ﾝﾞﾟ\-]*$/,
+  // こっちだと「あｧ」の時に正しい判定にならなかった
+  // zen: /^[^\x20-\x7e]*$/,
+  zen: /^[^\x01-\x7E\uFF61-\uFF9F]+$/,
+};
+
+const checkReg = (text, type) => {
+  const reg = new RegExp(REGS[type]);
+  return reg.test(text);
+};
+
+console.log(checkReg("002", "num"));
+console.log(checkReg("0a2", "numAlphabet"));
+console.log(checkReg("https://s-ishizaki.sakura.ne.jp/", "url"));
+console.log(checkReg("-1.2", "decimal2"));
+console.log(checkReg("あｧ", "zen"));
---
+++ snippet-memo/07-unique-array.js
@@ -0,0 +1,4 @@
+// 【参考】
+// https://qiita.com/netebakari/items/7c1db0b0cea14a3d4419
+
+// 【内容：配列の重複排除】
