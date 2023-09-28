c0f5a818880b3ad50382293ef9724f6bca2929c3
zakzakst <e1433104@gmail.com>
2023-09-28 21:05:53 +0900

    修正

---
+++ snippet-memo/16-geolocation-api.ts
@@ -0,0 +1,17 @@
+// 【参考】
+// 調べる
+
+// 【内容：】
+
+// ChatGPTのコード
+
+// if ('geolocation' in navigator) {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     const { latitude, longitude } = position.coords;
+//     console.log(`緯度: ${latitude}, 経度: ${longitude}`);
+//   }, (error) => {
+//     console.error('位置情報の取得に失敗しました', error);
+//   });
+// } else {
+//   console.log('このブラウザは位置情報をサポートしていません');
+// }
