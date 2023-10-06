7dfe4ed6fc1c1949d8c124057699672ff89b2731
zakzakst <e1433104@gmail.com>
2023-10-06 11:27:41 +0900

    修正

--- snippet-memo/01-20/20-yield.ts
+++ snippet-memo/01-20/20-yield.ts
@@ -1,9 +1,12 @@
 // 【参考】
 // https://ja.javascript.info/async-iterators-generators
 // https://tokitsubaki.com/javascript-yield-statement/676/
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/for-await...of
 
+// NOTE: ちょっと内容掴み切れないので保留
+
+// NOTE: ChatGPTで出力されたコード
 // 【内容：非同期ジェネレータを使用した反復処理】
-
 // async function* fetchMultipleData(urls) {
 //   for (const url of urls) {
 //     const response = await fetch(url);
@@ -13,7 +16,6 @@
 //     yield await response.json();
 //   }
 // }
-
 // const dataUrls = ['https://api.example.com/data1', 'https://api.example.com/data2'];
 // for await (const data of fetchMultipleData(dataUrls)) {
 //   console.log(data);
