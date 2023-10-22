c2e3888a4f00844c8008795598028eb8799e7d0e
zakzakst <e1433104@gmail.com>
2023-10-22 19:02:07 +0900

    split screen

--- src/split_screen/script.ts
+++ src/split_screen/script.ts
@@ -1,1 +1,28 @@
+const detailButtonEls = document.querySelectorAll(".js-detail-button");
+const modalEl = document.getElementById("js-modal");
+const closeEls = document.querySelectorAll(".js-close");
+const MODAL_ACTIVE_CLASS = "is-active";
 
+const openModal = () => {
+  modalEl.classList.add(MODAL_ACTIVE_CLASS);
+};
+
+const closeModal = () => {
+  modalEl.classList.remove(MODAL_ACTIVE_CLASS);
+};
+
+const init = () => {
+  [...detailButtonEls].forEach((el) => {
+    el.addEventListener("click", () => {
+      openModal();
+    });
+  });
+
+  [...closeEls].forEach((el) => {
+    el.addEventListener("click", () => {
+      closeModal();
+    });
+  });
+};
+
+init();
