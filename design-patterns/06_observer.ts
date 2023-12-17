21ab3a4b1bf9b106882935f7dc10780b221cd48e
zakzakst <e1433104@gmail.com>
2023-12-17 13:39:23 +0900

    observer

---
+++ design-patterns/06_observer.ts
@@ -0,0 +1,33 @@
+type ObservableFuncArg = string
+type ObservableFunc = (arg: ObservableFuncArg) => void
+
+class Observable {
+  observers: ObservableFunc[]
+
+  constructor() {
+    this.observers = []
+  }
+
+  subscribe(f: ObservableFunc) {
+    this.observers.push(f)
+  }
+
+  unsubscribe(f: ObservableFunc) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== f)
+  }
+
+  notify(arg: ObservableFuncArg) {
+    this.observers.forEach((observer) => observer(arg))
+  }
+}
+
+const observable = new Observable()
+const handleEvent = () => {
+  observable.notify('event')
+}
+const observerFunc = (arg: ObservableFuncArg) => {
+  console.log(`${Date.now()} ${arg}`)
+}
+observable.subscribe(observerFunc)
+
+handleEvent()
